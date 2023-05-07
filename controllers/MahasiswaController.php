c5311ac544368d087e5e52c81e3cbcf7f13fdba1
zilfi31 <zilfiana31@gmail.com>
2023-05-07 19:14:08 +0800

    first commit

---
+++ controllers/MahasiswaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+class MahasiswaController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    public function actionProfil()
+    {
+        return $this->render('profil');
+    }
+
+}
